23fdc54def5dafae85d98ae6bfc9833a35ab2bd0
Egor Marin <e.marin@enpicom.com>
2025-08-31 22:12:34 +0200

    fix install-sh with constant tmp path

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -u
 set -o pipefail
 
 echo "Running this will require sudo, please enter your password" && sudo ls > /dev/null
-cd /tmp && mkdir $(date +%Y%m%d%H%M%S) && cd $! && git clone https://github.com/marinegor/dotfiles.git --recursive && cd dotfiles
+rm -rf /tmp/marinegor-dotfiles && cd /tmp/marinegor-dotfiles && git clone https://github.com/marinegor/dotfiles.git --recursive && cd dotfiles
 sudo ./install -p dotbot-apt/apt.py -c apt.conf.yaml && ./install && bash -c "source ~/.bashrc && ./install -c tools.conf.yaml"
 cd
