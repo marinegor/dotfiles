c6f1755ea309eaf4ea141eddb484b6282d5f3f2f
Egor Marin <e.marin@enpicom.com>
2025-08-31 22:10:54 +0200

    create tmpdir with timestamp

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -u
 set -o pipefail
 
 echo "Running this will require sudo, please enter your password" && sudo ls > /dev/null
-cd /tmp && git clone https://github.com/marinegor/dotfiles.git --recursive && cd dotfiles
+cd /tmp && mkdir $(date +%Y%m%d%H%M%S) && cd $! && git clone https://github.com/marinegor/dotfiles.git --recursive && cd dotfiles
 sudo ./install -p dotbot-apt/apt.py -c apt.conf.yaml && ./install && bash -c "source ~/.bashrc && ./install -c tools.conf.yaml"
 cd
