9b8b0744389b4e0cddc00091bfbe6607dca447a7
Egor Marin <e.marin@enpicom.com>
2025-08-31 23:48:40 +0200

    add warning installation message before tools installation

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ set -o pipefail
 
 echo "Running this will require sudo, please enter your password" && sudo ls > /dev/null
 cd && git clone https://github.com/marinegor/dotfiles.git --recursive && cd dotfiles
-sudo ./install -p dotbot-apt/apt.py -c apt.conf.yaml && ./install && bash -c "source ~/.bashrc && ./install -c tools.conf.yaml"
+sudo ./install -p dotbot-apt/apt.py -c apt.conf.yaml && ./install && echo "Now installing tools, will take a while..." && bash -c "source ~/.bashrc && ./install -c tools.conf.yaml"
 cd
