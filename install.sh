4de0267c24c7f4a125e4934a8a9fcab6cb5cefc5
Egor Marin <e.marin@enpicom.com>
2025-09-05 18:36:50 +0200

    add linux condition

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ set -o pipefail
 
 echo "Running this will require sudo, please enter your password" && sudo ls > /dev/null
 cd && git clone https://github.com/marinegor/dotfiles.git --recursive && cd dotfiles
-sudo ./install -p dotbot-apt/apt.py -c apt.conf.yaml && ./install && echo "Now installing tools, will take a while..." && bash -c "source ~/.bashrc && ./install -c tools.conf.yaml"
+if $(uname -s | grep -q Linux); then sudo ./install -p dotbot-apt/apt.py -c apt.conf.yaml; fi
+./install && echo "Now installing tools, will take a while..." && bash -c "source ~/.bashrc && ./install -c tools.conf.yaml"
 cd
